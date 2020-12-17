dceb41394af36e47d6d171c58e9ea770e282d4b2
fazer1929 <abhishekagrawal8888@gmail.com>
2020-12-17 19:16:13 +0530

    added Proc file

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -10,12 +10,16 @@ urlpatterns = [
     path('register/', user_views.register,name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html') ,name='login'),
     path('password-reset/',
-     auth_views.PasswordResetView.as_view(template_name='users/password_reset.html') ,
-     name='password-reset'),
+    auth_views.PasswordResetView.as_view(template_name='users/password_reset.html') ,
+    name='password-reset'),
     
     path('password-reset/done/',
-     auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html') ,
-     name='password-reset-done'),
+    auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html') ,
+    name='password-reset-done'),
+    
+    path('password-reset-confirm/<uidb64>/<token>',
+    auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html') ,
+    name='password-reset-confirm'),
     
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html') ,name='logout'),
     path('profile/', user_views.profile ,name='profile'),
